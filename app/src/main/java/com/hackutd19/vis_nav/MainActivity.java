b19f7cdfd1094e292210580b20fbc36408db398d
giakhanh780f <john1909d@gmail.com>
2019-02-23 17:40:18 -0600

    Added Firebase vision libraries and starting work on implementing the camera preview

--- app/src/main/java/com/hackutd19/vis_nav/MainActivity.java
+++ app/src/main/java/com/hackutd19/vis_nav/MainActivity.java
@@ -1,5 +1,8 @@
 package com.hackutd19.vis_nav;
 
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.getSupportActionBar().hide();
+
+        CameraManager manager =
+                (CameraManager)getSystemService(CAMERA_SERVICE);
+        try {
+            for (String cameraId : manager.getCameraIdList()) {
+                CameraCharacteristics chars = manager.getCameraCharacteristics(cameraId);
+                // Do something with the characteristics
+                Integer facing = chars.get(CameraCharacteristics.LENS_FACING);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
